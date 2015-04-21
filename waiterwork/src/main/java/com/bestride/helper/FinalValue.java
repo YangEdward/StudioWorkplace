50bb93e0795791bcdacef40088d33e82be141a7b
YangEdward <murongyunlong@gmail.com>
2015-04-21 15:46:44 +0800

    更改内容，用于正式应用

--- waiterwork/src/main/java/com/bestride/helper/FinalValue.java
+++ waiterwork/src/main/java/com/bestride/helper/FinalValue.java
@@ -36,7 +36,7 @@ public class FinalValue {
 	
 	/*internet connect*/
 	public static final int SUCCESS_CODE = 20;
-//	private static final String IP_ADDRESS = "http://192.168.2.114:9090/shmp/pms/";
+//	private static final String IP_ADDRESS = "http://192.168.2.77:8080/shmp/pms/";
 	private static final String IP_ADDRESS = "http://192.168.2.200:8081/shmp/pms/";
 	public static final String BASE_URL = IP_ADDRESS + "fm/PAD/";
 	private static final String ROOM_URL = IP_ADDRESS + "rm/roomPAD/";
